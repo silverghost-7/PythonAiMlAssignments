eb274f11d8c2deb9e5d3fe88ffe8622f4bb5c091
Chaitanya Joshi <550.chaitanya@gmail.com>
2026-06-14 22:57:54 +0530

    Assignment 2: number operations

---
+++ Assignment2/NumberOperations.py
@@ -0,0 +1,12 @@
+num1 = 0
+num2 = 0
+
+print("Enter first number:")
+num1 = int(input())
+print("Enter second number:")
+num2 = int(input())
+
+print("Addition :",num1+num2)
+print("Subtraction :",num1-num2)
+print("Multiplication :",num1*num2)
+print("Division :",num1/num2)
